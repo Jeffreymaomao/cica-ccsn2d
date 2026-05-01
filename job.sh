b7efe271eda95d0241098241aa011cd53c781077
Chang-Mao <jeffrey0613mao@gmail.com>
2026-05-02 02:26:05 +0800

    job

--- job.sh
+++ job.sh
@@ -9,6 +9,23 @@
 
 cd ${SLURM_SUBMIT_DIR}
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/cluster/software/conda/py3.7_default/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/cluster/software/conda/py3.7_default/etc/profile.d/conda.sh" ]; then
+        . "/cluster/software/conda/py3.7_default/etc/profile.d/conda.sh"
+    else
+        export PATH="/cluster/software/conda/py3.7_default/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+conda activate node
+
 PNPM=/cluster/home/changmao/.local/share/pnpm/pnpm
 
 mkdir -p log
